d4abeda5ce65c9ec6ee6af48b8ad71a24d1ac97b
kevinadhiguna <kevin17016@mail.unpad.ac.id>
2021-09-17 16:20:04 +0700

    refactor: export class

--- src/student/student.controller.ts
+++ src/student/student.controller.ts
@@ -1,7 +1,7 @@
 import { Controller, Get } from '@nestjs/common';
 
 @Controller('students') // <- Tell NestJS that the class below is a controller.
-class StudentController {
+export class StudentController {
   @Get()
   getStudents() {
     return "Hello from GET students!";
