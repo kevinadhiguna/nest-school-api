df35ebe3e5bed6d929d91c80574637945f733701
kevinadhiguna <kevin17016@mail.unpad.ac.id>
2021-09-17 18:11:45 +0700

    refactor: create a function to handle POST requests

--- src/student/student.controller.ts
+++ src/student/student.controller.ts
@@ -1,7 +1,8 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post } from '@nestjs/common';
 
 @Controller('students') // <- Tell NestJS that the class below is a controller.
 export class StudentController {
+  // Handle GET requests
   @Get()
   getStudents() {
     return "All students!";
@@ -10,5 +11,11 @@ export class StudentController {
   @Get('/:studentId') // <- Append to URL, therefore it will be : '/students/:studentId'
   getStudentById() {
     return "Get a student by ID";
+  }
+
+  // Handle POST requests
+  @Post() 
+  createStudent() {
+    return "Create a Student";
   }
 }
