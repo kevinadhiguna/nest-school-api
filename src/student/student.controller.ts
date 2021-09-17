541cfb0cc22e0714abe68cd051ae10701b0743ea
kevinadhiguna <kevin17016@mail.unpad.ac.id>
2021-09-17 16:18:12 +0700

    refactor: create a function to handle GET requests
    /students

--- src/student/student.controller.ts
+++ src/student/student.controller.ts
@@ -1,6 +1,9 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 
 @Controller('students') // <- Tell NestJS that the class below is a controller.
 class StudentController {
-  
+  @Get()
+  getStudents() {
+    return "Hello from GET students!";
+  }
 }
