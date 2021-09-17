ccc9f82c08b06343355fd277d43a98c1b2f10da8
kevinadhiguna <kevin17016@mail.unpad.ac.id>
2021-09-17 18:52:35 +0700

    refactor: add a couple of controllers

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { StudentController } from '../student/student.controller';
+import { StudentTeacherController } from '../teacher/student.controller';
+import { TeacherController } from '../teacher/teacher.controller';
 
 @Module({
   imports: [],
-  controllers: [ StudentController ],
+  controllers: [ StudentController, StudentTeacherController, TeacherController ],
 })
 export class AppModule {}
