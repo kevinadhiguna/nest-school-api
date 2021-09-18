144c93a847968ab70be5056db52f2d6ad2683ce6
kevinadhiguna <kevin17016@mail.unpad.ac.id>
2021-09-18 16:49:34 +0700

    refactor: add and implement dto

--- src/student/student.controller.ts
+++ src/student/student.controller.ts
@@ -1,5 +1,7 @@
 import { Controller, Get, Post, Put, Param, Body } from '@nestjs/common';
 
+import { CreateStudentDto, UpdateStudentDto } from './dto/student.dto';
+
 @Controller('students') // <- Tell NestJS that the class below is a controller.
 export class StudentController {
   // Handle GET requests
@@ -16,7 +18,7 @@ export class StudentController {
 
   // Handle POST requests
   @Post()
-  createStudent(@Body() body) {
+  createStudent(@Body() body: CreateStudentDto) {
     console.log('Body:', body);
     // Convert a JSON object to a string
     return `Create a Student with the following data ${JSON.stringify(body)}`;
@@ -24,7 +26,7 @@ export class StudentController {
 
   // Handle PUT requests
   @Put('/:studentId')
-  updateStudent(@Param('studentId') studentId: string, @Body() body) {
+  updateStudent(@Param('studentId') studentId: string, @Body() body: UpdateStudentDto) {
     return `Update a Student with ID of ${studentId} with data of ${JSON.stringify(
       body,
     )}`;
