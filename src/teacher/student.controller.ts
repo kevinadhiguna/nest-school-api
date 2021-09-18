ec254109bed7f6b5880615bcc0b7799277935c11
kevinadhiguna <kevin17016@mail.unpad.ac.id>
2021-09-18 16:30:02 +0700

    refactor: add teacherId and studentId as parameters

--- src/teacher/student.controller.ts
+++ src/teacher/student.controller.ts
@@ -1,15 +1,18 @@
-import { Controller, Get, Put } from '@nestjs/common';
+import { Controller, Get, Param, Put } from '@nestjs/common';
 
 @Controller('/teachers/:teacherId/students')
 export class StudentTeacherController {
   @Get()
-  getStudents() {
-    return "Get Students that belong to a Teacher";
+  getStudents(@Param('teacherId') teacherId: string) {
+    return `Get Students that belong to a Teacher with ID ${teacherId}`;
   }
 
   // Handle PUT requests
   @Put('/:studentId')
-  updateStudentTeacher() {
-    return "Update Student Teacher";
+  updateStudentTeacher(
+    @Param('teacherId') teacherId: string,
+    @Param('studentId') studentId: string,
+  ) {
+    return `Update a Student with ID of ${studentId} to a Teacher with ID of ${teacherId}`;
   }
 }
