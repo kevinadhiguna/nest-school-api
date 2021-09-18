af9d3dbe87e09c842632a5d803fb5ad70add8e17
kevinadhiguna <kevin17016@mail.unpad.ac.id>
2021-09-18 17:07:05 +0700

    refactor(dto): implement some of students dto

--- src/teacher/student.controller.ts
+++ src/teacher/student.controller.ts
@@ -1,9 +1,11 @@
 import { Controller, Get, Param, Put } from '@nestjs/common';
 
+import { FindStudentResponseDto, StudentResponseDto } from '../student/dto/student.dto';
+
 @Controller('/teachers/:teacherId/students')
 export class StudentTeacherController {
   @Get()
-  getStudents(@Param('teacherId') teacherId: string) {
+  getStudents(@Param('teacherId') teacherId: string): FindStudentResponseDto[] {
     return `Get Students that belong to a Teacher with ID ${teacherId}`;
   }
 
@@ -12,7 +14,7 @@ export class StudentTeacherController {
   updateStudentTeacher(
     @Param('teacherId') teacherId: string,
     @Param('studentId') studentId: string,
-  ) {
+  ): StudentResponseDto {
     return `Update a Student with ID of ${studentId} to a Teacher with ID of ${teacherId}`;
   }
 }
