12e733e6584d244e2c7ed34caf6431337c56b0c8
kevinadhiguna <kevin17016@mail.unpad.ac.id>
2021-09-18 16:58:17 +0700

    refactor: implement variious DTOs

--- src/student/student.controller.ts
+++ src/student/student.controller.ts
@@ -1,24 +1,31 @@
 import { Controller, Get, Post, Put, Param, Body } from '@nestjs/common';
 
-import { CreateStudentDto, UpdateStudentDto } from './dto/student.dto';
+import {
+  FindStudentResponseDto,
+  CreateStudentDto,
+  UpdateStudentDto,
+  StudentResponseDto,
+} from './dto/student.dto';
 
 @Controller('students') // <- Tell NestJS that the class below is a controller.
 export class StudentController {
   // Handle GET requests
   @Get()
-  getStudents() {
+  getStudents(): FindStudentResponseDto[] {
     return 'All students!';
   }
 
   @Get('/:studentId') // <- Append to URL, therefore it will be : '/students/:studentId'
-  getStudentById(@Param('studentId') studentId: string) {
+  getStudentById(
+    @Param('studentId') studentId: string,
+  ): FindStudentResponseDto {
     console.log('Student ID:', studentId);
     return `Get a Student with ID of ${studentId}`;
   }
 
   // Handle POST requests
   @Post()
-  createStudent(@Body() body: CreateStudentDto) {
+  createStudent(@Body() body: CreateStudentDto): StudentResponseDto {
     console.log('Body:', body);
     // Convert a JSON object to a string
     return `Create a Student with the following data ${JSON.stringify(body)}`;
@@ -26,7 +33,10 @@ export class StudentController {
 
   // Handle PUT requests
   @Put('/:studentId')
-  updateStudent(@Param('studentId') studentId: string, @Body() body: UpdateStudentDto) {
+  updateStudent(
+    @Param('studentId') studentId: string,
+    @Body() body: UpdateStudentDto,
+  ): StudentResponseDto {
     return `Update a Student with ID of ${studentId} with data of ${JSON.stringify(
       body,
     )}`;
