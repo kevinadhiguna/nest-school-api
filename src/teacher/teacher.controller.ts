9a4339d94329fa9111c07d1ad88feab2f8f076db
kevinadhiguna <kevin17016@mail.unpad.ac.id>
2021-09-18 17:07:39 +0700

    refactor(dto): implement teacher dto

--- src/teacher/teacher.controller.ts
+++ src/teacher/teacher.controller.ts
@@ -1,15 +1,19 @@
 import { Controller, Get, Param } from '@nestjs/common';
 
+import { FindTeacherResponseDto } from '../teacher/dto/teacher.dto';
+
 @Controller('teachers')
 export class TeacherController {
   // Handle GET requests
   @Get()
-  getTeachers() {
+  getTeachers(): FindTeacherResponseDto[] {
     return 'All Teachers';
   }
 
   @Get('/:teacherId') // <- Append to URL, therefore it will be : '/teachers/:teacherId'
-  getTeacherById(@Param('teacherId') teacherId: string) {
+  getTeacherById(
+    @Param('teacherId') teacherId: string,
+  ): FindTeacherResponseDto {
     return `Get a Teacher with ID of ${teacherId}`;
   }
 }
