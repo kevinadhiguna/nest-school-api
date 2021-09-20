a82c3c7a17fb09f092befbb10e104d298cb5cba0
kevinadhiguna <kevin17016@mail.unpad.ac.id>
2021-09-20 10:12:06 +0700

    refactor: add a createStudent method

--- src/student/student.service.ts
+++ src/student/student.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@nestjs/common';
+import { v4 as uuid } from 'uuid';
+
 import { students } from './db';
-import { FindStudentResponseDto } from './dto/student.dto';
+import { CreateStudentDto, FindStudentResponseDto, StudentResponseDto } from './dto/student.dto';
 
 @Injectable()
 export class StudentService {
@@ -13,6 +15,16 @@ export class StudentService {
   getStudentById(studentId: string): FindStudentResponseDto {
     return this.students.find(student => {
       return student.id === studentId;
-    })
+    });
+  }
+
+  createStudent(payload: CreateStudentDto): StudentResponseDto {
+    let newStudent = {
+      id: uuid(),
+      ...payload,
+    };
+    // Add 'newStudent' to database (using push method to add it to an object)
+    this.students.push(newStudent);
+    return newStudent;
   }
 }
