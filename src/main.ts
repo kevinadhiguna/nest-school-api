dfac76bf92b3038e64563eb9c6e3e6292aeb9e12
kevinadhiguna <kevin17016@mail.unpad.ac.id>
2021-09-20 13:31:38 +0700

    refactor: add try-catch block and assign port

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
 
+const port = 3000;
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  try {
+    await app.listen(port);
+    console.log(
+      `NestJS School API has been launched at http://localhost:${port} !`,
+    );
+  } catch (err) {
+    console.log('An error occurred :', err);
+  }
 }
 bootstrap();
