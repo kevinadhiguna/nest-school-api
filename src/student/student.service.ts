a61e02abafefffab13c57a3ef9e65e61d40a5cd9
kevinadhiguna <kevin17016@mail.unpad.ac.id>
2021-09-20 09:56:09 +0700

    refactor: add return type to the getStudents method

--- src/student/student.service.ts
+++ src/student/student.service.ts
@@ -1,11 +1,12 @@
 import { Injectable } from '@nestjs/common';
 import { students } from './db';
+import { FindStudentResponseDto } from './dto/student.dto';
 
 @Injectable()
 export class StudentService {
   private students = students;
 
-  getStudents() {
+  getStudents(): FindStudentResponseDto[] {
     return this.students;
   }
 }
