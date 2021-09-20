439db763abd5f5169cae07323787b7d9c2eeb248
kevinadhiguna <kevin17016@mail.unpad.ac.id>
2021-09-20 11:54:08 +0700

    refactor(service): add getTeachers method

--- src/teacher/teacher.service.ts
+++ src/teacher/teacher.service.ts
@@ -1,4 +1,13 @@
 import { Injectable } from '@nestjs/common';
 
+import { teachers } from '../student/db';
+import { FindTeacherResponseDto } from './dto/teacher.dto';
+
 @Injectable()
-export class TeacherService {}
+export class TeacherService {
+  private teachers = teachers;
+
+  getTeachers(): FindTeacherResponseDto[] {
+    return this.teachers;
+  }
+}
