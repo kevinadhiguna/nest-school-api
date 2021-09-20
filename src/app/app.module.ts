ae61d63f7714d5ccb8b2b89dfd026616548565f8
kevinadhiguna <kevin17016@mail.unpad.ac.id>
2021-09-20 09:38:51 +0700

    refactor: add providers

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { StudentController } from '../student/student.controller';
 import { StudentTeacherController } from '../teacher/student.controller';
 import { TeacherController } from '../teacher/teacher.controller';
+import { StudentService } from '../student/student.service';
 
 @Module({
   imports: [],
   controllers: [StudentController, StudentTeacherController, TeacherController],
+  providers: [StudentService],
 })
 export class AppModule {}
