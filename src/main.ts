ff0ec449fbbfc552630baa718b1928e21bdc7cf7
kevinadhiguna <kevin17016@mail.unpad.ac.id>
2021-09-22 04:03:06 +0700

    refactor: catch at function call

--- src/main.ts
+++ src/main.ts
@@ -5,13 +5,9 @@ const port = 3000;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  try {
-    await app.listen(port);
-    console.log(
-      `NestJS School API has been launched at port ${port} !`,
-    );
-  } catch (err) {
-    console.log('An error occurred :', err);
-  }
+  await app.listen(port);
+  console.log(`NestJS School API has been launched at port ${port} !`);
 }
-bootstrap();
+bootstrap().catch(err => {
+  console.error('An error occured during app launch :', err);
+});
