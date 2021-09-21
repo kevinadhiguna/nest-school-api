18212bb0b77a6bcc07466f559d001c2df93c51d7
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2021-09-21 01:24:58 +0000

    refactor: remove unnecesary codes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,9 @@
 import { Module } from '@nestjs/common';
 
-// import { StudentController } from '../student/student.controller';
-// import { StudentTeacherController } from '../teacher/student.controller';
-// import { TeacherController } from '../teacher/teacher.controller';
-// import { StudentService } from '../student/student.service';
-// import { TeacherService } from '../teacher/teacher.service';
 import { TeacherModule } from 'src/teacher/teacher.module';
 import { StudentModule } from 'src/student/student.module';
 
 @Module({
   imports: [TeacherModule, StudentModule],
-  // controllers: [StudentController, StudentTeacherController, TeacherController],
-  // providers: [StudentService, TeacherService],
 })
 export class AppModule {}
