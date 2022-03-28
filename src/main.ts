50d87e395f04bf0f7919d04c69ca3584ee34664c
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2022-03-28 02:24:46 +0000

    refactor: fetch hostname from environment variables

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { AppModule } from './app/app.module';
 require('dotenv').config();
 
 const PORT = process.env.PORT;
+const HOSTNAME = process.env.HOSTNAME;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
